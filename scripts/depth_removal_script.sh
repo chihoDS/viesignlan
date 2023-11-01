8bd95612e713aac0cfb819fdb465d87bb1b66386
Chi Ho <chihods@gmail.com>
2023-11-02 01:50:17 +0700

    chore: change scripts because merging 2 processes

--- scripts/depth_removal_script.sh
+++ scripts/depth_removal_script.sh
@@ -1,1 +1,1 @@
-python src/data/depth_removal.py --depth_videos_input data/raw/AUTSL/train --log_file output/logs/log_remove_depth_videos.txt 
+python src/data/depth_removal.py --depth_videos_input data/raw/AUTSL/train --log_file outputs/logs/log_remove_depth_videos.txt 
--- scripts/keypoints_generation_script.sh
+++ scripts/keypoints_generation_script.sh
@@ -1,1 +1,1 @@
-python src/features/skeleton_keypoints_generation/keypoints_generation.py --log_file outputs/logs/log_val_keypoints_generation.txt --input_videos data/raw/AUTSL/val --output_keypoints data/interim/AUTSL/keypoints/val
+python src/features/wholepose_keypoints_generation/keypoints_generation.py --log_file outputs/logs/log_val_keypoints_generation.txt --videos_dir data/raw/AUTSL/val --keypoints_dir data/interim/AUTSL/keypoints/val
